139978f220d7a438fb6a7442b730102d9e792f8d
Jumpei Ogawa <phanective@gmail.com>
2018-10-07 03:51:46 +0900

    chore: add tests

--- tests/lib/rules/editorconfig.js
+++ tests/lib/rules/editorconfig.js
@@ -9,7 +9,7 @@
 // -----------------------------------------------------------------------------
 
 const rule = require("../../../lib/rules/editorconfig"),
-
+      path = require("path"),
       RuleTester = require("eslint").RuleTester;
 
 
@@ -17,20 +17,59 @@ const rule = require("../../../lib/rules/editorconfig"),
 // Tests
 // -----------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const configDir = path.join(__dirname, "../../configs"),
+      ruleTester = new RuleTester(),
+
+      combinations = {
+        default: {
+          filename: path.join(configDir, "default/target.js"),
+          validCode: `'use strict';
+console.log('failure');
+`,
+        },
+      };
+
 ruleTester.run("editorconfig", rule, {
-
   valid: [
-
-    // give me some code that won't trigger a warning
+    {
+      filename: combinations.default.filename,
+      code: combinations.default.validCode,
+    },
   ],
 
   invalid: [
     {
-      code: "    console.log(\"failure\");",
+      // Indents
+      filename: combinations.default.filename,
+      code: `'use strict';
+    console.log('failure');
+`,
+      output: combinations.default.validCode,
       errors: [{
-        message: "Fill me in.",
-        type: "Me too",
+        message: "EditorConfig: Expected indentation of 0 spaces but found 4.",
+        line: 2,
+        column: 2,
+      }],
+    },
+    {
+      // Trailing line break at EOF
+      filename: combinations.default.filename,
+      code: `'use strict';
+console.log('failure');`,
+      output: combinations.default.validCode,
+      errors: [{
+        message: "EditorConfig: Newline required at end of file but not found.",
+        line: 2,
+      }],
+    },
+    {
+      // Trailing whitespace
+      filename: combinations.default.filename,
+      code: "'use strict';" + "        \nconsole.log('failure');\n",
+      output: combinations.default.validCode,
+      errors: [{
+        message: "EditorConfig: Trailing spaces not allowed.",
+        line: 1,
       }],
     },
   ],
