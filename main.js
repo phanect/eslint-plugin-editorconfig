3b473146228b39c56ab1c233046125866f6a6932
Jumpei Ogawa <phanective@gmail.com>
2020-12-12 20:24:28 +0900

    feat: add plugin:editorconfig/all preset

--- main.js
+++ main.js
@@ -14,5 +14,20 @@ module.exports = {
         "unicode-bom": "off",
       },
     },
+    all: {
+      rules: {
+        "eol-last": "off",
+        indent: "off",
+        "linebreak-style": "off",
+        "no-trailing-spaces": "off",
+        "unicode-bom": "off",
+
+        "editorconfig/charset": "error",
+        "editorconfig/eol-last": "error",
+        "editorconfig/indent": "error",
+        "editorconfig/linebreak-style": "error",
+        "editorconfig/no-trailing-spaces": "error",
+      },
+    },
   },
 };
