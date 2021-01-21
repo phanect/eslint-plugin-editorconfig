75f9b78db177e4862fcca1f96d6871eb8cdecd8a
Jumpei Ogawa <phanective@gmail.com>
2021-01-21 21:42:14 +0900

    test: copy test package to run linter outside of this project
    test package has no dependencies except for eslint-plugin-editorconfig, but without copying the test package outside of this project, the test package is affected by node_modules under the project root.
    
    Before this change, eslint-plugin-typescript did not work without typescript package, but I could not be noticed about it because typescript package is already installed in the node_modules under the project root although typescript is not installed in the test package itself.

--- tests/package/test.sh
+++ tests/package/test.sh
@@ -2,12 +2,16 @@
 
 set -eu
 
-DIRNAME="$(dirname "${BASH_SOURCE[0]}")"
+TEST_PKG_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+PROJECT_ROOT="$(realpath "${TEST_PKG_DIR}/../..")"
+TMP="$(mktemp --directory)"
 
-cd "${DIRNAME}/../../"
+cd "${PROJECT_ROOT}"
 PACKAGE="$(npm pack .)"
 
-cd "${DIRNAME}"
-npm install "../../${PACKAGE}"
+# If you test without copying, test package is affected by node_modules in the project root
+cp --recursive "${TEST_PKG_DIR}" "${TMP}/"
+cd "${TMP}/package/"
+npm install "${PROJECT_ROOT}/${PACKAGE}"
 
 npm run lint
