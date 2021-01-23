c5b85ee0ba44b1d4ed49364ecd83e0a8d03dd41e
Jumpei Ogawa <phanective@gmail.com>
2021-01-23 20:00:54 +0900

    test: rewrite package test script

--- test-packages/test.sh
+++ test-packages/test.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-set -eu
+set -eux
 
-TEST_PKG_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-PROJECT_ROOT="$(realpath "${TEST_PKG_DIR}/../..")"
+TEST_PKGS_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
+PROJECT_ROOT="$(realpath "${TEST_PKGS_DIR}/..")"
 TMP="$(mktemp --directory)"
 
 cd "${PROJECT_ROOT}"
 PACKAGE="$(npm pack .)"
 
-# If you test without copying, test package is affected by node_modules in the project root
-cp --recursive "${TEST_PKG_DIR}" "${TMP}/"
-cd "${TMP}/package/"
-npm install "${PROJECT_ROOT}/${PACKAGE}"
+cd "${TEST_PKGS_DIR}"
+git clean -Xd --force
 
-npm run lint
+# If you test without copying, test package is affected by node_modules in the project root
+cp --recursive "${TEST_PKGS_DIR}" "${TMP}/"
+
+for PKGDIR in $(find "${TMP}/test-packages/" -maxdepth 1 -type d ! -path "${TMP}/test-packages/"); do
+  cd "${PKGDIR}"
+  npm install
+  npm install --save-dev "${PROJECT_ROOT}/${PACKAGE}"
+  npm run lint
+done
