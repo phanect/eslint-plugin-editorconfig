e341932a837547e1308e71bdbe026d672fcadadd
Jumpei Ogawa <phanective@gmail.com>
2021-10-17 15:40:23 +0900

    test: test both with ESLint 7 & 8

--- test-packages/test.sh
+++ test-packages/test.sh
@@ -15,9 +15,11 @@ git clean -Xd --force
 # If you test without copying, test package is affected by node_modules in the project root
 cp --recursive "${TEST_PKGS_DIR}" "${TMP}/"
 
-for PKGDIR in $(find "${TMP}/test-packages/" -maxdepth 1 -type d ! -path "${TMP}/test-packages/"); do
-  cd "${PKGDIR}"
-  npm install
-  npm install --save-dev "${PROJECT_ROOT}/${PACKAGE}"
-  npm run lint
+for ESLINT_VERSION in "7" "8"; do
+  for PKGDIR in $(find "${TMP}/test-packages/" -maxdepth 1 -type d ! -path "${TMP}/test-packages/"); do
+    cd "${PKGDIR}"
+    npm install
+    npm install --save-dev "eslint@${ESLINT_VERSION}" "${PROJECT_ROOT}/${PACKAGE}"
+    npm run lint
+  done
 done
