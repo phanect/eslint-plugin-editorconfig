0062d200da65f9f2e335a5fdfba033b31a3fcfbf
Jumpei Ogawa <git@phanective.org>
2024-01-04 09:37:14 +0900

    refactor: migrate to ESLint Stylistic & support Flat Config

--- src/base.ts
+++ src/base.ts
@@ -2,29 +2,59 @@
  * @file Report and fix EditorConfig rule violation.
  * @author Jumpei Ogawa
  */
+import { camelCase } from "change-case";
 import editorconfig from "editorconfig";
-import { Linter } from "eslint";
-import { klona } from "klona/lite";
 import { clone } from "./clone.ts";
 import type { BuildRule } from "./types.ts";
 import type { Rule } from "eslint";
 
 export const buildRule: BuildRule = async ({ baseRuleName, description, omitFirstOption, getESLintOption }) => {
-  const jsBaseRule: Rule.RuleModule = klona(new Linter().getRules().get(baseRuleName));
-  let tsBaseRules;
+  let jsBaseRule: Rule.RuleModule;
+  let tsBaseRule: Rule.RuleModule;
 
   try {
-    const { rules } = await import("@typescript-eslint/eslint-plugin");
-    tsBaseRules = rules;
+    jsBaseRule = (await import(`@stylistic/eslint-plugin-js/rules/${baseRuleName}`))[camelCase(baseRuleName)];
+
+    if (!jsBaseRule) {
+      const err: NodeJS.ErrnoException = new Error();
+      err.code = "ERR_INVALID_RULE_NAME";
+      throw err;
+    }
   } catch (err) {
-    if (err.code !== "MODULE_NOT_FOUND") {
+    if (
+      err.code === "MODULE_NOT_FOUND" ||
+      err.code === "ERR_PACKAGE_PATH_NOT_EXPORTED" ||
+      err.code === "ERR_INVALID_RULE_NAME"
+    ) {
+      throw new Error(`Could not import rule "${baseRuleName}" from @stylistic/eslint-plugin-js. This may be a bug of eslint-plugin-editorconfig. Sorry for the inconvenience.`);
+    } else {
+      throw err;
+    }
+  }
+
+  try {
+    tsBaseRule = (await import(`@stylistic/eslint-plugin-ts/rules/${baseRuleName}`)).default;
+
+    if (!tsBaseRule) {
+      const err: NodeJS.ErrnoException = new Error();
+      err.code = "ERR_INVALID_RULE_NAME";
+      throw err;
+    }
+  } catch (err) {
+    if (
+      err.code === "MODULE_NOT_FOUND" ||
+      err.code === "ERR_PACKAGE_PATH_NOT_EXPORTED" ||
+      err.code === "ERR_INVALID_RULE_NAME"
+    ) {
+      throw new Error(`Could not import rule "${baseRuleName}" from @stylistic/eslint-plugin-ts. This may be a bug of eslint-plugin-editorconfig. Sorry for the inconvenience.`);
+    } else {
       throw err;
     }
   }
 
   // Remove first option
   if (omitFirstOption !== false) {
-    jsBaseRule.meta.schema.shift();
+    jsBaseRule.meta?.schema?.shift();
   }
 
   return {
@@ -42,21 +72,7 @@ export const buildRule: BuildRule = async ({ baseRuleName, description, omitFirs
       const filename = context.getFilename();
       const ecParams = editorconfig.parseSync(context.getFilename(filename));
       const { enabled, eslintOption } = getESLintOption(ecParams);
-
-      let baseRule;
-
-      if (filename.endsWith(".ts")) {
-        if (!tsBaseRules) {
-          throw new Error("eslint-plugin-editorconfig requires typescript and @typescript-eslint/eslint-plugin to lint *.ts files. Run `npm install typescript @typescript-eslint/eslint-plugin`.");
-        } else if (!tsBaseRules?.[baseRuleName]) {
-          throw new Error(`Unexpected rule name "${baseRuleName}" not found in @typescript-eslint/eslint-plugin. Sorry, this may be a bug.`);
-        }
-
-        baseRule = klona(tsBaseRules[baseRuleName]);
-      } else {
-        baseRule = jsBaseRule;
-      }
-
+      const baseRule = filename.endsWith(".ts") ? tsBaseRule : jsBaseRule;
       const _context = eslintOption ? clone(context, { options: [ eslintOption, ...context.options ]}) : context;
 
       return enabled ? baseRule.create(_context) : {};
