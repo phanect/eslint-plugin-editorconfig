46eae96b7f8f25a44ed888f6f730713b60911a7b
Jumpei Ogawa <git@phanective.org>
2024-01-13 19:41:06 +0900

    fix: do not try to call rules from `@stylistic/eslint-plugin-ts` if rule does not exist

--- src/rules/eol-last.ts
+++ src/rules/eol-last.ts
@@ -3,6 +3,7 @@ import { buildRule } from "../base.js";
 export default await buildRule({
   baseRuleName: "eol-last",
   description: "Enforce EditorConfig rules for the newlines at the end of files",
+  useTsRule: false,
   getESLintOption: (ecParams) => {
     if (ecParams.insert_final_newline === true) {
       return { enabled: true, eslintOption: "always" };
--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,7 @@ type BuildRuleOptions = {
   baseRuleName: "unicode-bom" | "eol-last" | "indent" | "linebreak-style" | "no-trailing-spaces";
   description: string;
   omitFirstOption?: boolean;
+  useTsRule: boolean;
   getESLintOption: (ecParams: Props) => { enabled: boolean, eslintOption?: string | number };
 }
-export type BuildRule = ({ baseRuleName, description, omitFirstOption, getESLintOption }: BuildRuleOptions) => Promise<Rule.RuleModule>;
+export type BuildRule = ({ baseRuleName, description, omitFirstOption, useTsRule, getESLintOption }: BuildRuleOptions) => Promise<Rule.RuleModule>;
