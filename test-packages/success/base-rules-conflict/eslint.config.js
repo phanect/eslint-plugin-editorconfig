53f32b424651601df11548630029141d951f206e
Jumpei Ogawa <git@phanective.org>
2024-01-15 21:48:29 +0900

    test: migrate test packages to flat config

---
+++ test-packages/success/base-rules-conflict/eslint.config.js
@@ -0,0 +1,32 @@
+import epec from "eslint-plugin-editorconfig";
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+  epec.configs.noconflict,
+  {
+    files: [ "*.js", "*.ts" ],
+
+    languageOptions: {
+      ecmaVersion: 2020,
+    },
+    plugins: {
+      editorconfig: epec,
+    },
+    rules: {
+      "unicode-bom": [ "error", "never" ],
+      "eol-last": [ "error", "always" ],
+      indent: [ "error", 2, {
+        SwitchCase: 1,
+      }],
+      "linebreak-style": [ "error", "unix" ],
+      "no-trailing-spaces": [ "error", {
+        skipBlankLines: false,
+      }],
+      "editorconfig/charset": "error",
+      "editorconfig/eol-last": "error",
+      "editorconfig/indent": "error",
+      "editorconfig/linebreak-style": "error",
+      "editorconfig/no-trailing-spaces": "error",
+    },
+  },
+];
---
+++ test-packages/success/ts/eslint.config.js
@@ -0,0 +1,25 @@
+import parser from "@typescript-eslint/parser";
+import epec from "eslint-plugin-editorconfig";
+
+/** @type { import("eslint").Linter.FlatConfig[] } */
+export default [
+  epec.configs.noconflict,
+  {
+    files: [ "*.js", "*.ts" ],
+
+    languageOptions: {
+      parser,
+      sourceType: "module",
+    },
+    plugins: {
+      editorconfig: epec,
+    },
+    rules: {
+      "editorconfig/charset": "error",
+      "editorconfig/eol-last": "error",
+      "editorconfig/indent": "error",
+      "editorconfig/linebreak-style": "error",
+      "editorconfig/no-trailing-spaces": "error",
+    },
+  },
+];
