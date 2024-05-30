7a08cf147c421f17ab4e50c97ce5feba5c5d10d9
Jumpei Ogawa <git@phanective.org>
2024-05-30 22:22:50 +0900

    test: add eslint-disable-\* comment to a test fixture which includes errors

--- examples/flat-config/src/invalid.ts
+++ examples/flat-config/src/invalid.ts
@@ -3,6 +3,7 @@ import { fetch } from "undici";
 const res = await fetch("https://example.com");
 
 try {
+  // eslint-disable-next-line editorconfig/indent
     const data = await res.json(); // This line should be warned by this plugin
   console.log(data);
 } catch (err) {
