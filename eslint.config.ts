84a4ae98daf701e4a83e4635ff34b483ae22572e
Jumpei Ogawa <git@phanective.org>
2024-10-27 08:20:03 +0900

    chore(prj): migrate ESLint config to flat config
    To avoid TypeScript & ESLint errors, I also added `prepare` to build the plugin.

---
+++ eslint.config.ts
@@ -0,0 +1,41 @@
+import { core, nodejs, unbundled } from "@phanect/lint";
+import epec from "eslint-plugin-editorconfig";
+import type { Linter } from "eslint";
+
+const configs: Linter.Config[] = [
+  {
+    ignores: [
+      "**/dist/**",
+      "./test-packages/**",
+      "./workspace/src/rules/charset.ts", // ignore until this rule is released again.
+    ],
+  },
+
+  ...core,
+  ...nodejs,
+  ...unbundled,
+  epec.configs.all,
+
+  {
+    // Do not add `files: [ "*" ],` here.
+
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+
+  {
+    files: [ "**/*.ts" ],
+    rules: {
+      // TODO Add this rule to @phanect/lint later
+      "n/no-unpublished-import": [ "error", {
+        ignoreTypeImport: true,
+      }],
+    },
+  },
+];
+
+export default configs;
