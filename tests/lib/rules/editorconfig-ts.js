b271b997120cd4b0ee58c2e9f30b4f5bb9b3a31c
Jumpei Ogawa <git@phanective.org>
2024-10-27 12:32:17 +0900

    test: ESLint v9 ― migrate to ESM

--- tests/lib/rules/editorconfig-ts.js
+++ tests/lib/rules/editorconfig-ts.js
@@ -1,16 +1,7 @@
 "use strict";
 
-// -----------------------------------------------------------------------------
-// Requirements
-// -----------------------------------------------------------------------------
-
-const path = require("path");
-const RuleTester = require("eslint").RuleTester;
-
-
-// -----------------------------------------------------------------------------
-// Tests
-// -----------------------------------------------------------------------------
+import { join } from "node:path";
+import { RuleTester } from "eslint";
 
 const ruleTester = new RuleTester({
   parser: require.resolve("@typescript-eslint/parser"),
@@ -21,7 +12,7 @@ const ruleTester = new RuleTester({
 
 const commonValidTests = [
   {
-    filename: path.join(__dirname, "../../configs/default/target.ts"),
+    filename: join(import.meta.dirname, "../../configs/default/target.ts"),
     code: `'use strict';
 const foo: number = 0;
 `,
@@ -36,7 +27,7 @@ ruleTester.run("editorconfig/charset (typescript)", require("../../../lib/rules/
 ruleTester.run("editorconfig/eol-last (typescript)", require("../../../lib/rules/eol-last"), {
   valid: commonValidTests,
   invalid: [{
-    filename: path.join(__dirname, "../../configs/default/target.ts"),
+    filename: join(import.meta.dirname, "../../configs/default/target.ts"),
     code: `'use strict';
 const foo: number = 0;`,
     output: `'use strict';
@@ -54,7 +45,7 @@ ruleTester.run("editorconfig/indent (typescript)", require("../../../lib/rules/i
     ...commonValidTests,
     {
       // Passing Options (indent)
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       options: [{ VariableDeclarator: { var: 2, let: 2, const: 3 }}],
       code: `'use strict';
 const foo: string = 'foo',
@@ -79,7 +70,7 @@ var e: string = 'e',
   ],
   invalid: [
     {
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       code: `'use strict';
     const foo: number = 0;
 `,
@@ -94,7 +85,7 @@ const foo: number = 0;
     },
     {
       // Passing Options
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       options: [{ VariableDeclarator: { var: 2, let: 2, const: 3 }}],
       code: `'use strict';
 const foo: string = 'foo',
@@ -133,7 +124,7 @@ var e: string = 'e',
 ruleTester.run("editorconfig/linebreak-style (typescript)", require("../../../lib/rules/linebreak-style"), {
   valid: commonValidTests,
   invalid: [{
-    filename: path.join(__dirname, "../../configs/default/target.ts"),
+    filename: join(import.meta.dirname, "../../configs/default/target.ts"),
     code: "'use strict';\r\nconst foo: number = 0;\n",
     output: "'use strict';\nconst foo: number = 0;\n",
     errors: [{
@@ -147,7 +138,7 @@ ruleTester.run("editorconfig/no-trailing-space (typescript)", require("../../../
   valid: [
     ...commonValidTests,
     {
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       code:`'use strict';
 
 // comment
@@ -155,7 +146,7 @@ const foo: string = 'foo';`,
     },
     {
       // Passing Options
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       options: [{ skipBlankLines: true, ignoreComments: true }],
       code: [
         "'use strict';",
@@ -167,7 +158,7 @@ const foo: string = 'foo';`,
   ],
   invalid: [
     {
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       code: "'use strict';" + "        \nconst foo: number = 0;\n",
       output: `'use strict';
 const foo: number = 0;
@@ -179,7 +170,7 @@ const foo: number = 0;
     },
     {
       // Passing Options
-      filename: path.join(__dirname, "../../configs/default/target.ts"),
+      filename: join(import.meta.dirname, "../../configs/default/target.ts"),
       options: [{ skipBlankLines: true, ignoreComments: true }],
       code: [
         "'use strict';",
