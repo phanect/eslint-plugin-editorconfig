34f5435f3795a92d159837c6f72b522e6afc1646
Jumpei Ogawa <git@phanective.org>
2025-07-11 19:46:07 +0900

    refactor: fix typing error

--- src/rules/no-trailing-spaces.ts
+++ src/rules/no-trailing-spaces.ts
@@ -4,5 +4,5 @@ export default buildRule({
   baseRuleName: "no-trailing-spaces",
   description: "Enforce EditorConfig rules for trailing spaces",
   omitFirstOption: false,
-  getESLintOption: (ecParams) => ({ enabled: ecParams.trim_trailing_whitespace }),
+  getESLintOption: (ecParams) => ({ enabled: !!ecParams.trim_trailing_whitespace }),
 });
