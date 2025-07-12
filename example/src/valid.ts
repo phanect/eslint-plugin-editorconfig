afbfc2e7dfd0ccde4d56d4bac60335955a2531c2
Jumpei Ogawa <git@phanective.org>
2025-07-12 18:04:49 +0900

    chore(example): make all the files valid

--- example/src/valid.ts
+++ example/src/valid.ts
@@ -3,7 +3,7 @@ import { fetch } from "undici";
 const res = await fetch("https://example.com");
 
 try {
-    const data = await res.json(); // This line should be warned by this plugin
+  const data = await res.json(); // This line should be warned by this plugin
   console.log(data);
 } catch (err) {
   console.error(err);
